f1da088c93a208283bca5007c7c702d465f63113
jianwen612 <djw612@outlook.com>
2021-03-22 23:29:38 -0500

    request permission

--- android/src/main/java/com/reactlibrary/ScreenTimeModule.java
+++ android/src/main/java/com/reactlibrary/ScreenTimeModule.java
@@ -4,9 +4,11 @@ package com.reactlibrary;
 
 import android.app.AppOpsManager;
 import android.app.AppOpsManager.*;
+import android.content.Intent;
 import android.os.Process;
 import android.content.Context;
 import android.os.Build;
+import android.provider.Settings;
 
 import androidx.annotation.RequiresApi;
 
@@ -19,15 +21,17 @@ import java.util.Objects;
 
 import static android.app.AppOpsManager.MODE_ALLOWED;
 import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+import static androidx.core.content.ContextCompat.startActivity;
 
 
 public class ScreenTimeModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
-
+    private final Context context;
     public ScreenTimeModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        this.context = getReactApplicationContext();
     }
 
 
@@ -40,8 +44,8 @@ public class ScreenTimeModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
-        callback.invoke("permission:" + checkForPermission(Objects.requireNonNull(this.getCurrentActivity()).getApplicationContext()));
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+        getPermission();
+//        callback.invoke("permission:" + checkForPermission(Objects.requireNonNull(this.getCurrentActivity()).getApplicationContext()));
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -49,5 +53,11 @@ public class ScreenTimeModule extends ReactContextBaseJavaModule {
         AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
         int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), context.getPackageName());
         return mode == MODE_ALLOWED;
+    }
+
+    private void getPermission(){
+        System.out.println("get permission.");
+        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        startActivity(this.context, intent, null);
     }
 }
